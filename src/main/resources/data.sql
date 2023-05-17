865dfad3692234f8f63400d11f9a2de09bdf02fb
eilmadc@gmail.com <eilmadc@gmail.com>
2023-05-17 15:37:44 +0200

    .

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,6 @@
 
 create database datau;
 use datau;
-DROP TABLE IF EXISTS user_roles;
 DROP TABLE IF EXISTS roles;
 DROP TABLE IF EXISTS users;
 
@@ -15,18 +14,29 @@ CREATE TABLE users(
 	PRIMARY KEY(id)
 );
 
-/* password */
+/* user rol admin */
 INSERT INTO users (username,email, password, roles) VALUES ('admin','admin@admin.es', '$2a$10$asGJXftXsOCZOSjCp4CwHuHd2KqASramYmsnF6H/gNYtEQC7CVS3q','admin');
 
-/* passuser */
+/* user rol user */
 INSERT INTO users (username, email, password, roles) VALUES ('user','user@user.es', '$2a$10$4YstvNaXIjmgV8EyNsB4IuiwZD5A7fShh.onjMhQZ0aGXL7n31uzy','user');
 
 
 /* Tabla de roles */
 CREATE TABLE roles (
 	id int NOT NULL AUTO_INCREMENT,
-	rol char(15) DEFAULT NULL,
+	name char(15) DEFAULT NULL,
     PRIMARY KEY(id)
 );
 
-INSERT INTO roles (rol) VALUES('ROLE_ADMIN'),('ROLE_MODERATOR'),('ROLE_USER');
+INSERT INTO roles (name) VALUES('ROLE_ADMIN'),('ROLE_MODERATOR'),('ROLE_USER');
+
+/* Tabla de user roles */
+CREATE TABLE user_roles (
+	id int NOT NULL AUTO_INCREMENT,
+	user_id int DEFAULT NULL,
+	role_id int DEFAULT NULL,
+    PRIMARY KEY(id)
+);
+
+INSERT INTO user_roles (user_id, role_id) VALUES(1,1),(2,3);
+
