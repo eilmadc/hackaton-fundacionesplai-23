9fd8801a5d35cda21446b2e175eb48aa76daaa07
eilmadc@gmail.com <eilmadc@gmail.com>
2023-05-17 14:30:06 +0200

    fix data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,14 +1,32 @@
+
+create database datau;
+use datau;
+DROP TABLE IF EXISTS user_roles;
+DROP TABLE IF EXISTS roles;
 DROP TABLE IF EXISTS users;
 
-CREATE TABLE users (
-	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	username VARCHAR(255),
-	password VARCHAR(255),
-	role VARCHAR(255)
+/* Tabla de usuarios */
+CREATE TABLE users(
+	id int NOT NULL AUTO_INCREMENT,
+	username char(15) DEFAULT NULL,
+	email char(15) DEFAULT NULL,
+	password char(254) DEFAULT NULL,
+	roles char(15) DEFAULT NULL,
+	PRIMARY KEY(id)
 );
 
 /* password */
-INSERT INTO users (username, password, role) VALUES ('admin', '$2a$10$asGJXftXsOCZOSjCp4CwHuHd2KqASramYmsnF6H/gNYtEQC7CVS3q','admin');
+INSERT INTO users (username,email, password, roles) VALUES ('admin','admin@admin.es', '$2a$10$asGJXftXsOCZOSjCp4CwHuHd2KqASramYmsnF6H/gNYtEQC7CVS3q','admin');
 
 /* passuser */
-INSERT INTO users (username, password, role) VALUES ('user', '$2a$10$4YstvNaXIjmgV8EyNsB4IuiwZD5A7fShh.onjMhQZ0aGXL7n31uzy','user');
+INSERT INTO users (username, email, password, roles) VALUES ('user','user@user.es', '$2a$10$4YstvNaXIjmgV8EyNsB4IuiwZD5A7fShh.onjMhQZ0aGXL7n31uzy','user');
+
+
+/* Tabla de roles */
+CREATE TABLE roles (
+	id int NOT NULL AUTO_INCREMENT,
+	rol char(15) DEFAULT NULL,
+    PRIMARY KEY(id)
+);
+
+INSERT INTO roles (rol) VALUES('ROLE_ADMIN'),('ROLE_MODERATOR'),('ROLE_USER');
